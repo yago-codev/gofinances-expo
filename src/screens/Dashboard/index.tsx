0a1a200c412c821679f7f148c625c80e669befc9
yago-codev <yagomilano92@gmail.com>
2021-08-12 01:00:34 -0300

    adicionando icones com @expo/vector-icons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,24 @@ import * as S from './styles'
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/42048838?s=60&v=4' }}
+            />
+            <S.User>
+              <S.UserGreeting>
+                Olá,
+              </S.UserGreeting>
+              <S.UserName>
+                Yago
+              </S.UserName>
+            </S.User>
+          </S.UserInfo>
+          <S.IconPower name="power" />
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,53 @@
 import styled from 'styled-components/native'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.colors.background}
+  background-color: ${({ theme }) => theme.colors.background};
+`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
 `
 
-export const Title = styled.Text`
+export const UserWrapper = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 ${RFValue(24)}px;
+`
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`
+
+export const User = styled.View`
+  margin-left: 17px;
+`
+
+export const UserGreeting = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`
+
+export const UserName = styled.Text`
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`
+
+export const IconPower = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.secondary};
 `
